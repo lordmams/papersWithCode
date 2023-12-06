7ea0e3cf3d30f49278d47a487afe86c0eff7e249
mery977 <elalaoui.hassani.meryem@gmail.com>
2023-12-06 21:46:34 +0100

    ajout model category

---
+++ model/CategoryM.py
@@ -0,0 +1,25 @@
+class Category:
+
+
+
+    def __init__(self):
+
+
+        self.__category_id: int = None
+        self.__category_name: str = ""
+
+    def setCategoryId(self, categoryId: int) -> None:
+
+        self.__category_id = categoryId
+
+    def getCategoryId(self) -> int:
+
+        return self.__category_id
+
+    def setCategorytName(self, categoryName: str) -> None:
+
+        self.__category_name = categoryName
+
+    def getCategoryName(self) -> str:
+
+        return self.__category_name
